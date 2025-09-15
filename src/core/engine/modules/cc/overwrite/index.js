67f331c461f3361c58e4f3b54c3dc80a64dc9a67
Knox <yanbin.huang@chukong-inc.com>
2025-09-15 10:01:02 +0800

    refine code

--- src/core/engine/modules/cc/overwrite/index.js
+++ src/core/engine/modules/cc/overwrite/index.js
@@ -1,12 +1,3 @@
-const { join } = require('path');
-
-module.exports = function(ccm, info) {
+module.exports = function(ccm) {
     require('./widget-manager')(ccm);
-
-    // serialize
-    const serialize = require(join(info.editor, './builtin/engine/dist/editor-extends/utils/serialize/index'));
-    EditorExtends.serialize = serialize.serialize;
-    EditorExtends.serializeCompiled = serialize.serializeCompiled;
-    const deserialize = require(join(info.editor, './builtin/engine/dist/editor-extends/utils/deserialize'));
-    EditorExtends.deserializeFull = deserialize.deserializeFull;
 };
