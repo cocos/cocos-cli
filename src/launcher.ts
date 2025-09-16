7fd50fdbe8db2546a70218f75c7d9de3699360a5
Knox <yanbin.huang@chukong-inc.com>
2025-09-16 10:27:13 +0800

    fix launcher name error

--- src/launcher.ts
+++ src/launcher.ts
@@ -2,7 +2,6 @@
 import Project from './core/project';
 import Engine from './core/engine';
 import { startupAssetDB } from './core/assets';
-import { join, resolve } from 'path';
 
 class ProjectManager {
     create() {
@@ -34,9 +33,8 @@ class ProjectManager {
     }
 }
 
-
-
 export const projectManager = new ProjectManager();
+
 // 这是测试代码，不能使用单元测试，因为 jest 会捕获 require 然后不走 preload 的特殊处理,导致读不了 cc
 (async () => {
     const { engine } = require('../.user.json');
