4d3819387929650aac0fe4fd9c9efdebe5f48c6f
yanOO1497 <yuanyuan1497@foxmail.com>
2025-10-10 14:42:49 +0800

    add cocos.config.schema & add ci workflow

--- src/core/configuration/@types/cocos.config.d.ts
+++ src/core/configuration/@types/cocos.config.d.ts
@@ -3,6 +3,7 @@ import { EngineConfig } from '../../engine/@types/config';
 
 // 用于 schema 校验规则导出
 export interface COCOS_CONFIG {
+    version: string;
     builder: BuildConfiguration;
     import: ImportConfiguration;
     engine: EngineConfig;
