6c54a81e1869c6ec0c68e89c0d5798aeee8c4d7a
yanOO1497 <yuanyuan1497@foxmail.com>
2025-10-10 21:02:54 +0800

    add build api

--- src/api/builder/builder.ts
+++ src/api/builder/builder.ts
@@ -1,16 +1,23 @@
 import { ApiBase } from "../base/api-base";
 import { build } from '../../core/builder'
-import { TCreateJsonFileResult } from "../assets/scheme";
 import { HttpStatusCode, COMMON_STATUS, CommonResultType } from "../base/scheme-base";
-import { BuildExitCode, IBuildCommandOption } from "../../core/builder/@types/protected";
+import { BuildExitCode } from "../../core/builder/@types/protected";
 import BuildErrorMap from "../../core/builder/error-map";
+import { description, param, title, tool } from "../decorator/decorator";
+import { SchemeBuildOption, SchemeBuildOptionType } from "./schema";
 
-export class Builder extends ApiBase {
+export class BuilderApi extends ApiBase {
+    constructor() {
+        super();
+    }
     async init() {
 
     }
 
-    async build(options: IBuildCommandOption) {
+    @tool('build')
+    @title('构建项目')
+    @description('根据选项将项目构建成指定平台游戏包, 如项目内已经设置好构建选项，则不需要传入参数')
+    async build(@param(SchemeBuildOption) options?: SchemeBuildOptionType) {
         let code: HttpStatusCode = COMMON_STATUS.SUCCESS;
         const ret: CommonResultType<{ exitCode: number }> = {
             code: code,
