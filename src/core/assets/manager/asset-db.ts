26d62ce7175da9adbaa9dd5d7436d17768183b34
bofeng <bofeng.song@chukong-inc.com>
2025-11-05 17:09:25 +0800

    refine

--- src/core/assets/manager/asset-db.ts
+++ src/core/assets/manager/asset-db.ts
@@ -1,6 +1,6 @@
 'use strict';
 
-import { AssetDBRegisterInfo, IAsset, IAssetDBInfo } from '../@types/private';
+import { AssetDBRegisterInfo, IAsset, IAssetDBInfo, IAssetInfo, QueryAssetsOption } from '../@types/private';
 import * as assetdb from '@cocos/asset-db';
 import EventEmitter from 'events';
 import { ensureDirSync, existsSync } from 'fs-extra';
@@ -14,7 +14,9 @@ import Utils from '../../base/utils';
 import assetConfig from '../asset-config';
 import { compileEffect, startAutoGenEffectBin } from '../asset-handler';
 import scripting from '../../scripting';
-import { DBChangeType } from '../../scripting/packer-driver/asset-db-interop';
+import { AssetChangeInfo, DBChangeType } from '../../scripting/packer-driver/asset-db-interop';
+import assetQuery from './query';
+import { AssetActionEnum } from '@cocos/asset-db/libs/asset';
 
 const AssetDBPriority: Record<string, number> = {
     internal: 99,
@@ -756,7 +758,23 @@ async function afterStartDB(dbInfoMap: Record<string, IAssetDBInfo>) {
 
     // 如果 temp 目录不存在
     if (!existsSync(AssetDBManager.tempRoot)) {
-        
+        const assetChanges: AssetChangeInfo[] = [];
+        const options: QueryAssetsOption = {
+            ccType: 'cc.Script',
+        };
+        const assetInfos = assetQuery.queryAssetInfos(options, ['meta', 'url', 'file', 'importer', 'type']) as IAssetInfo[];
+        assetInfos.map((assetInfo) => {
+            if (assetInfo.importer === 'javascript' || assetInfo.importer === 'typescript') {
+                assetChanges.push({
+                    type: AssetActionEnum.add,
+                    uuid: assetInfo.uuid,
+                    filePath: assetInfo.file,
+                    importer: assetInfo.importer,
+                    userData: assetInfo.meta?.userData || {},
+                });
+            }
+        });
+        await scripting.compileScripts(assetChanges);
     }
     // 目前结构里，没有关闭数据库的逻辑
 }
