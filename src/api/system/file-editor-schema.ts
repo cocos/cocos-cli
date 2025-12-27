a800fa15b1b080cfd0690789b7f5de042787e9c3
Zhou Zhenglong <zlzhou.sh@outlook.com>
2025-12-27 14:16:59 +0800

    update describe

--- src/api/system/file-editor-schema.ts
+++ src/api/system/file-editor-schema.ts
@@ -6,7 +6,7 @@ const FILE_EXTENSIONS = ['js', 'ts', 'jsx', 'tsx', 'json',
 
 // Information for inserting content at line n of the file // 在文件第 n 行插入内容的信息
 export const SchemaInsertTextAtLineInfo = z.object({
-    dbURL: z.string().describe('File name to modify'), // 需要修改文件名
+    dbURL: z.string().describe('Asset URL of the file to be modified'), // 需要修改的文件的资产URL
     fileType: z.enum(FILE_EXTENSIONS).describe('File type'), // 文件类型
     lineNumber: z.number().min(1).default(1).describe('Line number (starting from 1)'), // 行号(从1开始)
     text: z.string().describe('Text content to insert'), // 需要插入的文本内容
@@ -14,7 +14,7 @@ export const SchemaInsertTextAtLineInfo = z.object({
 
 // Delete content from startLine to endLine of the file // 删除文件的第 startLine 到 endLine 行的内容
 export const SchemaEraseLinesInRangeInfo = z.object({
-    dbURL: z.string().describe('File name to modify'), // 需要修改文件名
+    dbURL: z.string().describe('Asset URL of the file to be modified'), // 需要修改的文件的资产URL
     fileType: z.enum(FILE_EXTENSIONS).describe('File type'), // 文件类型
     startLine: z.number().min(1).default(1).describe('Start deleting from startLine'), // 从第 startLine 行开始删除
     endLine: z.number().min(1).default(1).describe('End deleting at endLine (endLine is also deleted)'), // 从第 endLine 行结束删除(endLine也删除)
@@ -22,7 +22,7 @@ export const SchemaEraseLinesInRangeInfo = z.object({
 
 // Replace target text in file with replacement text // 替换文件的 目标文本 为 替换文本
 export const SchemaReplaceTextInFileInfo = z.object({
-    dbURL: z.string().describe('File name to modify'), // 需要修改文件名
+    dbURL: z.string().describe('Asset URL of the file to be modified'), // 需要修改的文件的资产URL
     fileType: z.enum(FILE_EXTENSIONS).describe('File type'), // 文件类型
     targetText: z.string().nonempty().describe('Target text'), // 目标文本
     replacementText: z.string().describe('Replacement text'), // 替换文本
@@ -30,7 +30,7 @@ export const SchemaReplaceTextInFileInfo = z.object({
 }).describe('Replace target text (text or regex) in file with replacement text'); // 替换文件的 目标文本（文本或者正则表达式） 为 替换文本
 
 export const SchemaQueryFileTextInfo = z.object({
-    dbURL: z.string().describe('File name to query'), // 需要查询文件名
+    dbURL: z.string().describe('Asset URL of the file to be queried'), // 需要查询文件的资产URL
     fileType: z.enum(FILE_EXTENSIONS).describe('File type'), // 文件类型
     startLine: z.number().min(1).default(1).describe('Start querying from startLine (default starts from line 1)'), // 从第 startLine 行开始查询(默认从第1行开始)
     lineCount: z.number().default(-1).describe('Number of lines to query starting from startLine (negative number for all lines, default -1)'), // 从第 startLine 行开始查询的行数(负数为全部行，默认-1)
