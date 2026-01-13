960c88306c49e9883675aa7f5360dccc55b1ea7e
fengyuzhe <song2008_2001@126.com>
2026-01-13 14:53:25 +0800

    test

--- src/core/scene/process-rpc/process-rpc.ts
+++ src/core/scene/process-rpc/process-rpc.ts
@@ -163,7 +163,13 @@ export class ProcessRPC<TModules extends Record<string, any>> {
             const { module, method, args } = msg;
             const target = this.handlers[module];
             if (target && typeof target[method] === 'function') {
-                target[method](...(args || []));
+                // console.log(`[ProcessRPC] notify: ${module}.${method}`, args);
+                try {
+                    target[method](...(args || []));
+                } catch (e) {
+                    console.error(`[ProcessRPC] Error handling notify ${module}.${method}:`, e);
+                    throw e;
+                }
             }
         }
     }
