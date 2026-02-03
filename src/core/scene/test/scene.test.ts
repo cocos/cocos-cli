b4f6bb3ad85f70083c04165e87dc9f7430e2fd9d
Zhou Zhenglong <zlzhou.sh@outlook.com>
2026-02-03 12:56:46 +0800

    test scene first one

--- src/core/scene/test/scene.test.ts
+++ src/core/scene/test/scene.test.ts
@@ -1,27 +1,27 @@
-// import * as fse from 'fs-extra';
-// import { SceneTestEnv } from './scene-test-env';
+import * as fse from 'fs-extra';
+import { SceneTestEnv } from './scene-test-env';
 
-// function removeCache() {
-//     if (fse.existsSync(SceneTestEnv.cacheDirectory)) {
-//         fse.removeSync(SceneTestEnv.cacheDirectory);
-//         fse.removeSync(SceneTestEnv.cacheDirectory + '.meta');
-//         console.log('删除场景测试目录:', SceneTestEnv.cacheDirectory);
-//     }
-// }
+function removeCache() {
+    if (fse.existsSync(SceneTestEnv.cacheDirectory)) {
+        fse.removeSync(SceneTestEnv.cacheDirectory);
+        fse.removeSync(SceneTestEnv.cacheDirectory + '.meta');
+        console.log('删除场景测试目录:', SceneTestEnv.cacheDirectory);
+    }
+}
 
-// beforeAll(async () => {
-//     removeCache();
-//     fse.ensureDirSync(SceneTestEnv.cacheDirectory);
-//     console.log('创建场景测试目录:', SceneTestEnv.cacheDirectory);
-//     const TestUtils = await import('../../test/global-setup');
-//     await TestUtils.globalSetup();
-// });
+beforeAll(async () => {
+    removeCache();
+    fse.ensureDirSync(SceneTestEnv.cacheDirectory);
+    console.log('创建场景测试目录:', SceneTestEnv.cacheDirectory);
+    const TestUtils = await import('../../test/global-setup');
+    await TestUtils.globalSetup();
+});
 
-// afterAll(async () => {
-//     removeCache();
-// });
+afterAll(async () => {
+    removeCache();
+});
 
-// import './editor-proxy-scene.testcase';
+import './editor-proxy-scene.testcase';
 // import './editor-proxy-prefab.testcase';
 // import './node-proxy.testcase';
 // import './component-proxy.testcase';
