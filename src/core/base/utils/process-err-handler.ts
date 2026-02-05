7af50942b8c853babff2351bac31247f55254c85
fengyuzhe <song2008_2001@126.com>
2026-02-05 11:55:18 +0800

    dd

--- src/core/base/utils/process-err-handler.ts
+++ src/core/base/utils/process-err-handler.ts
@@ -36,8 +36,10 @@ export function setupProcessHandler(process: NodeJS.Process | ChildProcess) {
 
     process.on('exit', (code) => {
         if (code !== 0) {
+            Error.stackTraceLimit = 50;
             console.error('bf test 000 进程异常退出，退出码:', code);
-            console.trace();
+            const error = new Error();
+            console.error(error.stack);
         }
     });
 }
