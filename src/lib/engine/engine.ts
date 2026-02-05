3a7a3309ea9e8deb80297123c0d5827515fb1c2c
Zhou Zhenglong <zlzhou.sh@outlook.com>
2026-02-05 14:40:01 +0800

    add scripting

--- src/lib/engine/engine.ts
+++ src/lib/engine/engine.ts
@@ -1,16 +1,8 @@
 import { GlobalPaths } from '../../global';
-import scripting from '../../core/scripting';
 
 export class Engine {
     static async init(projectPath: string): Promise<void> {
         const { initEngine } = await import('../../core/engine');
         return await initEngine(GlobalPaths.enginePath, projectPath);
-    }
-    static async initScripting(projectPath: string): Promise<void> {
-        const { Engine } = await import('../../core/engine');
-        return await scripting.initialize(
-            projectPath,
-            GlobalPaths.enginePath,
-            Engine.getConfig().includeModules);
     }
 }
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,3 +3,4 @@ export * from './base/base';
 export * from './configuration/configuration';
 export * from './engine/engine';
 export * from './project/project';
+export * from './scripting/scripting';
