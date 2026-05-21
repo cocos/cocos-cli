f43c2a9eaabdb2c8f91c5d41aea306ceb3d1579d
bofeng <bofeng.song@chukong-inc.com>
2026-05-21 11:26:03 +0800

    Fix memory leak.

--- src/core/scene/scene-process/service/gizmo/node/transform.ts
+++ src/core/scene/scene-process/service/gizmo/node/transform.ts
@@ -41,6 +41,9 @@ class TransformGizmo extends GizmoBase<Component> {
     private _gizmo: TransformBaseGizmo;
     protected updateControllerTransform?(): void;
 
+    private static _activeInstances = new Set<TransformGizmo>();
+    private static _sharedEventMap: { [key: string]: () => void } | null = null;
+
     constructor(target: Component | null) {
         super(target);
         const toolName = getService()?.Gizmo?.transformToolData?.toolName ?? 'position';
@@ -80,24 +83,30 @@ class TransformGizmo extends GizmoBase<Component> {
         this._gizmo.show();
     }
 
-    private _eventMap: { [key: string]: () => void } = {};
-
-    private _initEventMap() {
-        if ('toolNameChanged' in this._eventMap) return;
-        this._eventMap.toolNameChanged = () => {
-            const tn = getService()?.Gizmo?.transformToolData?.toolName ?? 'position';
-            this.changeTool(tn as TransformToolDataToolNameType);
-        };
-        this._eventMap.viewModeChanged = () => {
-            repaintEngine();
-        };
-        this._eventMap.pivotChanged = () => {
-            (this._gizmo as any).updateControllerTransform?.();
-            repaintEngine();
-        };
-        this._eventMap.coordinateChanged = () => {
-            (this._gizmo as any).updateControllerTransform?.();
-            repaintEngine();
+    private static _ensureSharedEventMap() {
+        if (TransformGizmo._sharedEventMap) return;
+        TransformGizmo._sharedEventMap = {
+            toolNameChanged: () => {
+                const tn = getService()?.Gizmo?.transformToolData?.toolName ?? 'position';
+                for (const inst of TransformGizmo._activeInstances) {
+                    inst.changeTool(tn as TransformToolDataToolNameType);
+                }
+            },
+            viewModeChanged: () => {
+                repaintEngine();
+            },
+            pivotChanged: () => {
+                const toolName = getService()?.Gizmo?.transformToolData?.toolName ?? 'position';
+                const gizmo = gizmoMap[toolName as TransformToolDataToolNameType];
+                (gizmo as any)?.updateControllerTransform?.();
+                repaintEngine();
+            },
+            coordinateChanged: () => {
+                const toolName = getService()?.Gizmo?.transformToolData?.toolName ?? 'position';
+                const gizmo = gizmoMap[toolName as TransformToolDataToolNameType];
+                (gizmo as any)?.updateControllerTransform?.();
+                repaintEngine();
+            },
         };
     }
 
@@ -120,23 +129,22 @@ class TransformGizmo extends GizmoBase<Component> {
             super.onShow();
         }
 
-        this._initEventMap();
-
         const svc = getService();
         const toolName = svc?.Gizmo?.transformToolData?.toolName ?? 'position';
         this.changeTool(toolName as TransformToolDataToolNameType);
 
-        const ttd = svc?.Gizmo?.transformToolData;
-        // 先移除旧监听，防止重复 onShow 导致监听器累积
-        ttd?.removeListener?.('tool-name-changed', this._eventMap.toolNameChanged);
-        ttd?.removeListener?.('view-mode-changed', this._eventMap.viewModeChanged);
-        ttd?.removeListener?.('pivot-changed', this._eventMap.pivotChanged);
-        ttd?.removeListener?.('coordinate-changed', this._eventMap.coordinateChanged);
+        const wasEmpty = TransformGizmo._activeInstances.size === 0;
+        TransformGizmo._activeInstances.add(this);
 
-        ttd?.addListener?.('tool-name-changed', this._eventMap.toolNameChanged);
-        ttd?.addListener?.('view-mode-changed', this._eventMap.viewModeChanged);
-        ttd?.addListener?.('pivot-changed', this._eventMap.pivotChanged);
-        ttd?.addListener?.('coordinate-changed', this._eventMap.coordinateChanged);
+        if (wasEmpty) {
+            TransformGizmo._ensureSharedEventMap();
+            const ttd = svc?.Gizmo?.transformToolData;
+            const em = TransformGizmo._sharedEventMap!;
+            ttd?.addListener?.('tool-name-changed', em.toolNameChanged);
+            ttd?.addListener?.('view-mode-changed', em.viewModeChanged);
+            ttd?.addListener?.('pivot-changed', em.pivotChanged);
+            ttd?.addListener?.('coordinate-changed', em.coordinateChanged);
+        }
 
         this._gizmo.onShow?.();
     }
@@ -146,12 +154,19 @@ class TransformGizmo extends GizmoBase<Component> {
             super.onHide();
         }
 
-        const svc = getService();
-        const ttd = svc?.Gizmo?.transformToolData;
-        ttd?.removeListener?.('tool-name-changed', this._eventMap.toolNameChanged);
-        ttd?.removeListener?.('view-mode-changed', this._eventMap.viewModeChanged);
-        ttd?.removeListener?.('pivot-changed', this._eventMap.pivotChanged);
-        ttd?.removeListener?.('coordinate-changed', this._eventMap.coordinateChanged);
+        TransformGizmo._activeInstances.delete(this);
+
+        if (TransformGizmo._activeInstances.size === 0) {
+            const svc = getService();
+            const ttd = svc?.Gizmo?.transformToolData;
+            const em = TransformGizmo._sharedEventMap;
+            if (em) {
+                ttd?.removeListener?.('tool-name-changed', em.toolNameChanged);
+                ttd?.removeListener?.('view-mode-changed', em.viewModeChanged);
+                ttd?.removeListener?.('pivot-changed', em.pivotChanged);
+                ttd?.removeListener?.('coordinate-changed', em.coordinateChanged);
+            }
+        }
 
         this._gizmo.onHide?.();
     }
