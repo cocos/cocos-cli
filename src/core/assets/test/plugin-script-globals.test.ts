648bb2e63e8b3cde534f78f30bca743552a8cfd9
shi <1611699255@qq.com>
2026-08-28 16:00:34 +0800

    fix(asset): restore plugin script global aliases

---
+++ src/core/assets/test/plugin-script-globals.test.ts
@@ -0,0 +1,25 @@
+import { DEFAULT_SIMULATED_GLOBALS, resolveSimulatedGlobals } from '../asset-handler/assets/utils/plugin-script-globals';
+
+describe('plugin script globals', () => {
+    test('uses the default aliases when no custom aliases are stored', () => {
+        expect(resolveSimulatedGlobals(undefined)).toEqual(DEFAULT_SIMULATED_GLOBALS);
+    });
+
+    test('appends custom aliases after the defaults', () => {
+        expect(resolveSimulatedGlobals(['PINK_PLUGIN_ALIAS'])).toEqual([
+            ...DEFAULT_SIMULATED_GLOBALS,
+            'PINK_PLUGIN_ALIAS',
+        ]);
+    });
+
+    test('treats legacy boolean metadata as no custom aliases', () => {
+        expect(resolveSimulatedGlobals(true)).toEqual(DEFAULT_SIMULATED_GLOBALS);
+    });
+
+    test('deduplicates default and custom aliases', () => {
+        expect(resolveSimulatedGlobals(['window', 'PINK_PLUGIN_ALIAS', 'PINK_PLUGIN_ALIAS'])).toEqual([
+            ...DEFAULT_SIMULATED_GLOBALS,
+            'PINK_PLUGIN_ALIAS',
+        ]);
+    });
+});
